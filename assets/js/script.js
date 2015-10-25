4626645ad9f6f29a46f7c81efdb19eecbe582fdf
Giorgia Willits <gwillits@berkeley.edu>
2015-10-24 21:16:53 -0700

    coffe and donut search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,38 @@
 var map;
+var service;
 var pos;
 var clickTime = "282 2nd Street 4th floor, San Francisco, CA 94105"
+var infoWindow;
 
 function initMap() {
   var directionsService = new google.maps.DirectionsService;
   var directionsDisplay = new google.maps.DirectionsRenderer;
-  var map = new google.maps.Map(document.getElementById('map'), {
+
+  // Initialize map
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
     zoom: 10
   });
   directionsDisplay.setMap(map);
   directionsDisplay.setPanel(document.getElementById("directions"));
-  var infoWindow = new google.maps.InfoWindow({map: map});
+  infoWindow = new google.maps.InfoWindow({map: map});
+  service = new google.maps.places.PlacesService(map);
+
 
   // Try HTML5 geolocation.
+  getCurrentPosition();
+
+  // Search for Coffee & Donuts nearby
+  addressToLocation(clickTime, getCoffeeAndDonuts);
+
+  // Add event listener to calculate directions
+  document.getElementById('travel-mode').addEventListener('change', function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  });
+
+}
+
+function getCurrentPosition() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       pos = {
@@ -30,19 +49,6 @@ function initMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
-
-  var onChangeHandler = function() {
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-  };
-  document.getElementById('start').addEventListener('change', onChangeHandler);
-  document.getElementById('end').addEventListener('change', onChangeHandler);
-
-  document.getElementById('travel-mode').addEventListener('change', function() {
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-  });
-
-
-
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -50,6 +56,73 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setContent(browserHasGeolocation ?
                         'Error: The Geolocation service failed.' :
                         'Error: Your browser doesn\'t support geolocation.');
+}
+
+function addressToLocation(address, callback) {
+	var geocoder = new google.maps.Geocoder();
+	geocoder.geocode( {address: address}, function(results, status) {
+			var resultLocations = [];
+			if(status == google.maps.GeocoderStatus.OK) {
+				if(results) {
+					for(var i = 0; i < results.length; i++) {
+						var result = results[i];
+						resultLocations.push( {
+								text:result.formatted_address,
+								addressStr:result.formatted_address,
+								location:result.geometry.location,
+                lat:result.geometry.location.lat(),
+                lng:result.geometry.location.lng()
+							});
+					};
+				}
+			} else if(status == google.maps.GeocoderStatus.ZERO_RESULTS) {
+				// address not found
+			}
+			if(resultLocations.length > 0) {
+				callback(resultLocations);
+			} else {
+				callback(null);
+			}
+		}
+	);
+}
+
+function getCoffeeAndDonuts(locations){
+  var requestCoffee = {
+    location: locations[0],
+    radius: '500',
+    query: 'coffee',
+    openNow: true
+  };
+  var requestDonuts = {
+    location: locations[0],
+    radius: '500',
+    query: 'donut',
+    openNow: true
+  };
+  service.textSearch(requestCoffee, placeMarkers);
+}
+
+function placeMarkers(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+        var place = results[i];
+        var image = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        var marker = new google.maps.Marker({
+          map: map,
+          icon: image,
+          title: place.name,
+          position: place.geometry.location
+        });
+    }
+  }
 }
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
@@ -82,5 +155,11 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
 
 
 
-$(document).ready(function() {
-});
+//$(document).ready(function() {
+//});
+
+  // var onChangeHandler = function() {
+  //   calculateAndDisplayRoute(directionsService, directionsDisplay);
+  // };
+  //document.getElementById('start').addEventListener('change', onChangeHandler);
+  //document.getElementById('end').addEventListener('change', onChangeHandler);

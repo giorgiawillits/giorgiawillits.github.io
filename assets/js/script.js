94ba34fe9d2a1acdedd826d360fbd3c8e937785b
Giorgia Willits <gwillits@berkeley.edu>
2015-10-25 23:00:28 -0700

    closest coffee aand donut

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 var map;
-var service;
-var pos;
-var clickTime = "282 2nd Street 4th floor, San Francisco, CA 94105"
+var currentPos;
+var clickTime;
 var infoWindow;
+var coffeeIcon;
+var donutIcon;
+var coffeeOrigins;
+var donutOrigins;
+var coffeeLocations;
+var donutLocations;
+var closeIcon;
 
 function initMap() {
   var directionsService = new google.maps.DirectionsService;
@@ -11,19 +17,24 @@ function initMap() {
   // Initialize map
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -34.397, lng: 150.644},
-    zoom: 10
+    zoom: 11
   });
   directionsDisplay.setMap(map);
   directionsDisplay.setPanel(document.getElementById("directions"));
   infoWindow = new google.maps.InfoWindow({map: map});
-  service = new google.maps.places.PlacesService(map);
 
 
   // Try HTML5 geolocation.
   getCurrentPosition();
 
   // Search for Coffee & Donuts nearby
-  addressToLocation(clickTime, getCoffeeAndDonuts);
+  coffeeIcon = 'https://chart.googleapis.com/chart?' +
+      'chst=d_map_pin_letter&chld=C|FF0000|000000';
+  donutIcon = 'https://chart.googleapis.com/chart?' +
+      'chst=d_map_pin_letter&chld=D|FFFF00|000000';
+  closeIcon = 'https://chart.googleapis.com/chart?' +
+      'chst=d_map_pin_letter&chld=A|FFFFF0|000000';
+  addressToLocation("282 2nd Street 4th floor, San Francisco, CA 94105", getCoffeeAndDonuts);
 
   // Add event listener to calculate directions
   document.getElementById('travel-mode').addEventListener('change', function() {
@@ -35,13 +46,13 @@ function initMap() {
 function getCurrentPosition() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      pos = {
+      currentPos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      infoWindow.setPosition(pos);
+      infoWindow.setPosition(currentPos);
       infoWindow.setContent('Location found.');
-      map.setCenter(pos);
+      map.setCenter(currentPos);
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
@@ -79,6 +90,7 @@ function addressToLocation(address, callback) {
 				// address not found
 			}
 			if(resultLocations.length > 0) {
+        clickTime = resultLocations[0]
 				callback(resultLocations);
 			} else {
 				callback(null);
@@ -88,6 +100,7 @@ function addressToLocation(address, callback) {
 }
 
 function getCoffeeAndDonuts(locations){
+  console.log("getCoffeeAndDonuts");
   var requestCoffee = {
     location: locations[0],
     radius: '500',
@@ -100,36 +113,141 @@ function getCoffeeAndDonuts(locations){
     query: 'donut',
     openNow: true
   };
-  service.textSearch(requestCoffee, placeMarkers);
+  var placesService = new google.maps.places.PlacesService(map);
+  placesService.textSearch(requestCoffee, placeCoffeeMarkers);
+  placesService.textSearch(requestDonuts, placeDonutMarkers);
 }
 
-function placeMarkers(results, status) {
+function placeCoffeeMarkers(results, status) {
+  console.log("placeCoffeeMarkers");
+  coffeeOrigins = [];
+  coffeeLocations = [];
+  for (var i = 0; i < results.length; i++){
+    coffeeLocations.push(results[i]);
+    coffeeOrigins.push(results[i].geometry.location);
+  }
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
         var place = results[i];
-        var image = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-
         var marker = new google.maps.Marker({
           map: map,
-          icon: image,
+          icon: coffeeIcon,
           title: place.name,
           position: place.geometry.location
         });
+    }
+  }
+  console.log("start CoffeeDistanceMatrixService");
+  var matrixService = new google.maps.DistanceMatrixService();
+  matrixService.getDistanceMatrix(
+    {
+      origins: coffeeOrigins,
+      destinations: [clickTime],
+      travelMode: google.maps.TravelMode.WALKING,
+    }, getClosestCoffee);
+}
+
+function placeDonutMarkers(results, status) {
+  console.log("placeDonutMarkers");
+  donutOrigins = [];
+  donutLocations = [];
+  for (var i = 0; i < results.length; i++){
+    donutLocations.push(results[i]);
+    donutOrigins.push(results[i].geometry.location);
+  }
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+        var place = results[i];
+        var marker = new google.maps.Marker({
+          map: map,
+          icon: donutIcon,
+          title: place.name,
+          position: place.geometry.location
+        });
+    }
+  }
+  console.log("start DonutDistanceMatrixService");
+  var matrixService = new google.maps.DistanceMatrixService();
+  matrixService.getDistanceMatrix(
+    {
+      origins: donutOrigins,
+      destinations: [clickTime],
+      travelMode: google.maps.TravelMode.WALKING,
+    }, getClosestDonuts);
+
+}
+
+function getClosestCoffee(response, status) {
+  console.log("getClosestCoffee");
+  if (status == google.maps.DistanceMatrixStatus.OK) {
+    var origins = response.originAddresses;
+    var minDistance = Infinity;
+    var closeCoffee = null;
+    for (var i = 0; i < origins.length; i++) {
+      var results = response.rows[i].elements;
+      for (var j = 0; j < results.length; j++) {
+        var element = results[j];
+        var distance = element.distance.value;
+        if (distance < minDistance) {
+          minDistance = distance;
+          closeCoffee = coffeeLocations[i];
+        }
+      }
+    }
+    if (closeCoffee != null) {
+      var infowindow = new google.maps.InfoWindow({
+        content: closeCoffee.name
+      });
+      var marker = new google.maps.Marker({
+        map: map,
+        title: closeCoffee.name,
+        position: closeCoffee.geometry.location
+      });
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+      console.log(closeCoffee.name);
+    }
+  }
+}
+
+function getClosestDonuts(response, status) {
+  console.log("getClosestDonuts");
+  if (status == google.maps.DistanceMatrixStatus.OK) {
+    var origins = response.originAddresses;
+    var minDistance = Infinity;
+    var closeDonut = null;
+    for (var i = 0; i < origins.length; i++) {
+      var results = response.rows[i].elements;
+      for (var j = 0; j < results.length; j++) {
+        var element = results[j];
+        var distance = element.distance.value;
+        if (distance < minDistance) {
+          minDistance = distance;
+          closeDonut = donutLocations[i];
+        }
+      }
+    }
+    if (closeDonut != null) {
+      var infowindow = new google.maps.InfoWindow({
+        content: closeDonut.name
+      });
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: closeIcon,
+        title: closeDonut.name,
+        position: closeDonut.geometry.location
+      });
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+      console.log(closeDonut.name);
     }
   }
 }
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   var selectedMode = document.getElementById("travel-mode").value
-  console.log(pos)
-  console.log(clickTime)
-  console.log(selectedMode)
   directionsService.route({
     origin: pos,
     destination: clickTime,
@@ -163,3 +281,11 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   // };
   //document.getElementById('start').addEventListener('change', onChangeHandler);
   //document.getElementById('end').addEventListener('change', onChangeHandler);
+
+        // var image = {
+        //   url: place.icon,
+        //   size: new google.maps.Size(71, 71),
+        //   origin: new google.maps.Point(0, 0),
+        //   anchor: new google.maps.Point(17, 34),
+        //   scaledSize: new google.maps.Size(25, 25)
+        // };
